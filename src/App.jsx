3f3a982596b5f25e37382f8c199cf5d152c3439b
dusan chmelko <dusan.chmelko@yahoo.com>
2025-08-01 21:49:04 +0200

    add react-query, supabase, first cabin table

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import GlobalStyles from "./styles/GlobalStyles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import AppLayout from "./ui/AppLayout";
 import Dashboard from "./pages/Dashboard";
@@ -11,9 +12,21 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 
+import GlobalStyles from "./styles/GlobalStyles";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            // staleTime: 60 * 1000
+            staleTime: 0
+        }
+    }
+});
+
 const App = () => {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
             <GlobalStyles />
             <BrowserRouter>
                 <Routes>
@@ -34,7 +47,7 @@ const App = () => {
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
             </BrowserRouter>
-        </>
+        </QueryClientProvider>
     );
 };
 
