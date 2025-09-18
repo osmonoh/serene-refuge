b840756e8c2db86c35099d6e92d95f54b009f2f4
dusan chmelko <dusan.chmelko@yahoo.com>
2025-09-18 23:01:19 +0200

    refactor chart start data

--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -28,89 +28,46 @@ const ChartBox = styled.div`
     }
 `;
 
-const startDataLight = [
+const startData = [
     {
         duration: "1 night",
         value: 0,
-        color: "#ef4444"
+        color: { dark: "#b91c1c", light: "#ef4444" }
     },
     {
         duration: "2 nights",
         value: 0,
-        color: "#f97316"
+        color: { dark: "#c2410c", light: "#f97316" }
     },
     {
         duration: "3 nights",
         value: 0,
-        color: "#eab308"
+        color: { dark: "#a16207", light: "#eab308" }
     },
     {
         duration: "4-5 nights",
         value: 0,
-        color: "#84cc16"
+        color: { dark: "#4d7c0f", light: "#84cc16" }
     },
     {
         duration: "6-7 nights",
         value: 0,
-        color: "#22c55e"
+        color: { dark: "#15803d", light: "#22c55e" }
     },
     {
         duration: "8-14 nights",
         value: 0,
-        color: "#14b8a6"
+        color: { dark: "#0f766e", light: "#14b8a6" }
     },
     {
         duration: "15-21 nights",
         value: 0,
-        color: "#3b82f6"
+        color: { dark: "#1d4ed8", light: "#3b82f6" }
     },
     {
         duration: "21+ nights",
         value: 0,
-        color: "#a855f7"
-    }
-];
-
-const startDataDark = [
-    {
-        duration: "1 night",
-        value: 0,
-        color: "#b91c1c"
-    },
-    {
-        duration: "2 nights",
-        value: 0,
-        color: "#c2410c"
-    },
-    {
-        duration: "3 nights",
-        value: 0,
-        color: "#a16207"
-    },
-    {
-        duration: "4-5 nights",
-        value: 0,
-        color: "#4d7c0f"
-    },
-    {
-        duration: "6-7 nights",
-        value: 0,
-        color: "#15803d"
-    },
-    {
-        duration: "8-14 nights",
-        value: 0,
-        color: "#0f766e"
-    },
-    {
-        duration: "15-21 nights",
-        value: 0,
-        color: "#1d4ed8"
-    },
-    {
-        duration: "21+ nights",
-        value: 0,
-        color: "#7e22ce"
+        color: { dark: "#7e22ce", light: "#a855f7" }
     }
 ];
 
@@ -144,7 +101,6 @@ const prepareData = (startData, stays) => {
 
 const DurationChart = ({ confirmedStays }) => {
     const { isDarkMode } = useDarkMode();
-    const startData = isDarkMode ? startDataDark : startDataLight;
     const data = prepareData(startData, confirmedStays);
 
     return (
@@ -162,13 +118,18 @@ const DurationChart = ({ confirmedStays }) => {
                         cy="50%"
                         paddingAngle={3}
                     >
-                        {data.map((entry) => (
-                            <Cell
-                                key={entry.duration}
-                                fill={entry.color}
-                                stroke={entry.color}
-                            />
-                        ))}
+                        {data.map((entry) => {
+                            const color = isDarkMode
+                                ? entry.color.dark
+                                : entry.color.light;
+                            return (
+                                <Cell
+                                    key={entry.duration}
+                                    fill={color}
+                                    stroke={color}
+                                />
+                            );
+                        })}
                     </Pie>
                     <Tooltip />
                     <Legend
